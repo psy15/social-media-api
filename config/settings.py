1ffa48dccff1811a9e39e3f57c6ed4bca1de487b
Paras <parasy152@gmail.com>
2022-10-15 23:44:15 +0530

    debug

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wrob-vs%spg+ls+j%0gp8xy$-77c_6g6!8=^+4-58p+uktlpy7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["https://damp-savannah-69789.herokuapp.com/"]
 
