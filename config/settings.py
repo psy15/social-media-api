2979b708cced600c3791ac1a17f32f1f989b38d6
Paras <parasy152@gmail.com>
2022-10-15 23:41:04 +0530

    some chngs

--- config/settings.py
+++ config/settings.py
@@ -68,6 +68,7 @@ SPECTACULAR_SETTINGS = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
